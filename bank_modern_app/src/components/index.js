6a6dc8a229e7d84910f6976b91169e3ad63f4214
oluwaseun <adunmofestus@gmail.com>
2024-01-23 03:13:18 +0100

    basic structures

---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./components/Billing";
+import Business from "./components/Business";
+import Button from "./components/Button";
+import CardDeal from "./components/CardDeal";
+import Client from "./components/Client";
+import CTA from "./components/CTA";
+import FeedBackCard from "./components/FeedBackCard";
+import Footer from "./components/Footer";
+import GetStarted from "./components/GetStarted";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+
+export {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Client,
+  CTA,
+  FeedBackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+};
