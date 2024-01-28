34a7daba7cb3867dec12e4b4e8326b53ba794c37
oluwaseun <adunmofestus@gmail.com>
2024-01-28 23:11:52 +0100

    work oo

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
 import styles from "./style";
-
-import {Billing, Business, CardDeal, Client, CTA, Footer, Hero, Navbar, Stats, Testimonials} from "./components";
-
+import { Billing, Business, CardDeal, Client, CTA, Footer, Hero, Navbar, Stats, Testimonials } from "./components";
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          <Navbar/>
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Hero/>
+          <Hero />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`} style={{ overflowX: 'hidden', overflowY: 'scroll', WebkitOverflowScrolling: 'touch' }}>
         <div className={`${styles.boxWidth}`}>
-          <Stats/>
-          <Business/>
-          <Billing/>
-          <CardDeal/>
-          <Testimonials/>
-          <Client/>
-          <CTA/>
-          <Footer/>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Client />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
